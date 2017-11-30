a94a1267968cc238ff0d3c29019dec9a38afc1f4
bluffingtuna@gmail.com <bluffingtuna@gmail.com>
2017-11-30 01:26:36 -0800

    parse algo optimized

---
+++ bin/blast.js
@@ -0,0 +1,24 @@
+#!/usr/bin/env node
+var fs = require('fs');
+var blast = require('..');
+var concat = require('concat-stream');
+
+// TODO: find a more reliable way to detect empty stdin
+if (process.stdin.isTTY) {
+  withoutPipe();
+} else{
+  withPipe();
+}
+
+function withPipe() {
+  function callback(str){
+    process.stdout.write(JSON.stringify(blast.parse(str)));
+  }
+  var con = concat(callback);
+  process.stdin.pipe(con);
+}
+
+function withoutPipe() {
+   console.log('ERROR: no content was piped');
+   process.exit(1);
+}
